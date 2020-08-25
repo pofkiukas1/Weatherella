383afffa86f032cbaa3b882f22a3ed50d3e16c49
pofkiukas1 <45460884+pofkiukas1@users.noreply.github.com>
2020-08-25 22:37:23 +0300

    updated data fixtures

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -8,41 +8,56 @@ use Faker\Factory;
 class ProductFixtures extends Fixture
 {
 
-
-
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create();
-        $name = [
-            'Umbrella',
-            'Hat',
-            'Shorts',
-            'Jacket',
-            'Coat',
-            'Raincoat',
-            'T-Shirt'
+        //$sampleData[element][n] possible values for n:  0 = name, 1 shortend for generating barcode(sku) , 2 applicable for weather
+        $sameplData = [
+            ['Umbrella', 'UM', 'light-rain, moderate-rain, heavy-rain, sleet, cold, warm'],
+            ['Hat','HAT','light-rain, clear, moderate-rain, heavy-rain, sleet, light-snow, moderate-snow, heavy-snow, isolated-clouds, cold, fog'],
+            ['Bonnet', 'BON','clear, warm, isolated-clouds, light-rain'],
+            ['Shorts','ST','warm, clear, isolated-clouds, scattered-clouds, overcast, fog'],
+            ['Jacket','JT','cold, clear, isolated-clouds, scattered-clouds, overcast, fog, light-rain'],
+            ['Coat','CT','cold, clear, isolated-clouds, scattered-clouds, overcast, light-snow, moderate-snow, heavy-snow, light-rain, moderate-rain'],
+            ['Raincoat','RCT','cold, warm, light-rain, moderate-rain, heavy-rain'],
+            ['T-Shirt', 'TSH','warm, clear, isolated-clouds, scattered-clouds, overcast, fog']
         ];
-    
-        $short = [
-            'UM',
-            'HAT',
-            'ST',
-            'JT',
-            'CT',
-            'RCT',
-            'TSH'
-        ];
-        
+       
         for ($i = 0; $i < 50; $i++) {
             $Product = new Product();
-            $productName = $faker->randomElement($name);
-            $Product->setSku($short[array_search($productName,$name,true)]);
+            $dataIndex = $faker->numberBetween($min = 0, $max = count($sameplData)-1);
+            $productName = $this->appendStrings($sameplData[$dataIndex][0], $faker->colorName());
+            $sku = $this->formSKU($sameplData[$dataIndex][1], $faker->unique()->numberBetween($min = 0, $max = 150));
+            $Product->setSku($sku);
             $Product->setName($productName);
             $Product->setPrice($faker->randomFloat($nbMaxDecimals = 2, $min = 0.01, $max = 150));
+            $Product->setAppropriateWeather($sameplData[$dataIndex][2]);
             $manager->persist($Product);
         }
 
 
         $manager->flush();
     }
+    /*
+    forms a sku using 2 strings 
+    */
+    function formSKU ($str1, $str2) { 
+
+        $str1 .='-';
+        $str1 .=$str2; 
+        
+        return $str1; 
+    } 
+    /*
+    appends 2 strings 
+    */
+    function appendStrings ($str1, $str2) { 
+
+        $str1 .=' ';
+        $str1 .=$str2; 
+        
+        return $str1; 
+    } 
+
+      
 }
